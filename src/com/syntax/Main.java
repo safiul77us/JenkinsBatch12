df136ea4f397c1ccc735df8602caebf8a7802d97
Safiul Alam <safiul77us@gmail.com>
2022-06-22 18:37:35 -0400

    added new code

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
         for (int i = 0; i < 5; i++) {
             System.out.println("hello! I am a cat");
+            System.out.println("hello! I am a dog");
         }
+
     }
 }
